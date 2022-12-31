4be0c269e1283b515962fed1ba4293b483f4074b
Diyorbek Olimov <diyorbekolimov2000@gmail.com>
2022-12-31 09:43:34 +0500

    solved new problems

--- 4kyu.js
+++ 4kyu.js
@@ -1,5 +1,5 @@
 // Snail
-// Given an n x n array, return the array elements arranged from outermost elements 
+// Given an n x n array, return the array elements arranged from outermost elements
 // to the middle element, traveling clockwise.
 // https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
 
@@ -13,3 +13,59 @@ const snail = function (array) {
   }
   return finalArray;
 };
+
+// ----------------------------------------------------------------------------------------
+
+// Human readable duration format
+// Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+// The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+// https://www.codewars.com/kata/52742f58faf5485cae000b9a
+
+function formatDuration(seconds) {
+  let time = { year: 31536000, day: 86400, hour: 3600, minute: 60, second: 1 },
+    res = [];
+
+  if (seconds === 0) return "now";
+
+  for (let key in time) {
+    if (seconds >= time[key]) {
+      let val = Math.floor(seconds / time[key]);
+      res.push((val += val > 1 ? " " + key + "s" : " " + key));
+      seconds = seconds % time[key];
+    }
+  }
+
+  return res.length > 1
+    ? res.join(", ").replace(/,([^,]*)$/, " and" + "$1")
+    : res[0];
+}
+
+// ------------------------------------------------------------------------------------
+
+// Range Extraction
+// A format for expressing an ordered list of integers is to use a comma separated list of either
+// individual integers
+// or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'. 
+// The range includes all integers in the interval including both endpoints. It is not considered a range unless it spans at least 3 numbers. For example "12,13,15-17"
+// Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
+// https://www.codewars.com/kata/51ba717bb08c1cd60f00002f
+
+function solution(list) {
+  const result = [];
+  let str = [];
+  for (let i = 0; i < list.length; i++) {
+    if (list[i + 1] === list[i] + 1) {
+      str.push(list[i]);
+    } else {
+      if (str.length < 2) {
+        result.push(...str, list[i]);
+      } else {
+        result.push(str[0] + "-" + list[i]);
+      }
+      str = [];
+    }
+  }
+  return result.join();
+}
+
+// ----------------------------------------------------------------------------------------------
--- 5kyu.js
+++ 5kyu.js
@@ -48,5 +48,34 @@ function generatingHashtag(str) {
   return "#" + newStr;
 }
 
-
 // ---------------------------------------------------------------------------------------------------------------
+
+// Human Readable Time
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+// https://www.codewars.com/kata/52685f7382004e774f0001f7
+
+function humanReadable(seconds) {
+  if (seconds < 0 || seconds > 360000) {
+    return null;
+  }
+
+  let hours = Math.floor(seconds / 3600);
+  seconds -= hours * 3600;
+
+  if (hours < 10) hours = "0" + hours;
+
+  let minutes = Math.floor(seconds / 60);
+  seconds -= minutes * 60;
+
+  if (minutes < 10) minutes = "0" + minutes;
+
+  if (seconds < 10) seconds = "0" + seconds;
+
+  return hours + ":" + minutes + ":" + seconds;
+}
+
+// -------------------------------------------------------------------------------------------------

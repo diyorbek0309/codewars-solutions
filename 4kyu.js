8642e2d593cb5484db926110a6534ebf797c8562
Diyorbek Olimov <diyorbekolimov2000@gmail.com>
2023-01-08 02:03:58 +0500

    solved some problems

--- 4kyu.js
+++ 4kyu.js
@@ -515,3 +515,29 @@ function parseInt(string) {
 }
 
 // --------------------------------------------------------------------------------------------
+
+// Square into Squares. Protect trees!
+// https://www.codewars.com/kata/54eb33e5bc1a25440d000891
+
+function decompose(n) {
+  let result = decomposer(n, n * n);
+  return result == null ? null : result.slice(0, result.length - 1);
+}
+
+function decomposer(n, remain) {
+  if (remain == 0) {
+    return [n];
+  }
+  for (let i = n - 1; i > 0; i--) {
+    if (remain - i * i >= 0) {
+      let r = decomposer(i, remain - i * i);
+      if (r != null) {
+        r.push(n);
+        return r;
+      }
+    }
+  }
+  return null;
+}
+
+// -----------------------------------------------------------------------------------------
